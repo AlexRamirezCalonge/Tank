8304a8c6d5191cfcfa89bd916254ad5e1da558b3
AlexRamirezCalonge <alex.ramirez.calonge@gmail.com>
2021-02-11 17:52:05 +0100

    Check function

--- robot.js
+++ robot.js
@@ -17,29 +17,48 @@ async function main(tank) {
       await tank.drive(0, 0);
     }
   }
+  //Check the location of enemies
+  async function check(tank, numTan1, location){
+    var numTan1;
+    var location;
+    for(var i = 0; i<=360; i=i+10){
+      numTan1 = await tank.scan(i, 10);
+      if(location==null && numTan1!=0){
+        location = i + 10;        
+      }
+      else if (location!=null){
+        break;
+      }
+    }
+    await tank.shoot(location, 700);
+    await tank.drive(0, 30);
+    await tank.shoot(location, 700);
+
+    location = null;
+    numTan1 = null;
+
+    check
+  }
 
   // main loop
 
   while (true) {
     // go up
     while (await tank.getY() < 900) {
-      await tank.drive(90, 50);
+      await check(tank);
+      //await tank.drive(location, 50);
+      //await tank.scan(0, 10);
+      //await tank.drive(0, distancia);
     }
     // shoot
-    await tank.shoot(270, 700);
+    await tank.shoot(0, 700);
     await fireLeftOrRight(tank);
     // go down
     while (await tank.getY() > 100) {
-      await tank.drive(270, 50);
+      await tank.drive(270, 0);
     }
     // shoot
     await tank.shoot(90, 700);
     await fireLeftOrRight(tank);
-
-    //Comentarios
-    await tank.getX();
-    await tank.getY();
-    await tank.getDamage();
-    await tank.getSpeed();
   }
 }
